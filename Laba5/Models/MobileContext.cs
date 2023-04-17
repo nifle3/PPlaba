b73078b27874149c45e7c53225bca57eaae05346
nifie <kuper116rus.2005@gmail.com>
2023-04-17 10:35:48 +0300

    add context

---
+++ Laba5/Models/MobileContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Laba5.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> dbContextOptions)
+            : base(dbContextOptions) 
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
